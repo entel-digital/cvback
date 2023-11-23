317e034805109a65541de0798929876716e71e56
Paula Vallejo <paula.vallejo@gmail.com>
2023-11-22 23:11:02 -0300

    wip events

--- cvback/events/models.py
+++ cvback/events/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django_jsonform.models.fields import ArrayField
 #from django.contrib.postgres.fields import ArrayField
 #from django_better_admin_arrayfield.models.fields import ArrayField
-from devices.models import Camera
+from devices.models import Camera, InferenceComputer
 from django.core.exceptions import ValidationError
 from django.utils.translation import gettext_lazy as _
 
@@ -25,3 +25,33 @@ class AreaOfInterest(models.Model):
 
     def __str__(self):
         return f"{self.camera} > {self.name}"
+
+class BoundingBox(models.Model):
+    added_date = models.DateTimeField("date created", auto_now_add=True)
+    top_left = ArrayField(models.FloatField(validators=[validate_relative]), size=2)
+    bottom_right = ArrayField(models.FloatField(validators=[validate_relative]), size=2)
+    type = models.CharField(max_length=255)
+    confidence = models.FloatField(validators=[validate_relative])
+
+class Inference(models.Model):
+    added_date = models.DateTimeField("date created", auto_now_add=True)
+    inference_computer = models.ForeignKey(InferenceComputer, on_delete=models.DO_NOTHING)
+    bounding_boxes = models.ForeignKey(BoundingBox, on_delete=models.DO_NOTHING)
+
+class Event(models.Model):
+    added_date = models.DateTimeField("date created", auto_now_add=True)
+    camera = models.ForeignKey(Camera, on_delete=models.CASCADE)
+    inference = models.ForeignKey(Inference, on_delete=models.DO_NOTHING)
+
+class TelegramAlert(models.Model):
+    added_date = models.DateTimeField("date created", auto_now_add=True)
+    telegram_user = models.CharField(max_length=255)
+    message = models.CharField(max_length=255)
+    events = models.ForeignKey(Event, on_delete=models.DO_NOTHING)
+
+class SMSAlert(models.Model):
+    added_date = models.DateTimeField("date created", auto_now_add=True)
+    phone_number = models.CharField(max_length=255)
+    message = models.CharField(max_length=255)
+    events = models.ForeignKey(Event, on_delete=models.DO_NOTHING)
+
