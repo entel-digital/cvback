8fa64a5131bacfa09dd307db64a00d3da73e936c
Paula Vallejo <paula.vallejo@gmail.com>
2024-05-20 20:44:56 -0400

    del admin.site(Alert)

--- cvback/events/admin.py
+++ cvback/events/admin.py
@@ -3,10 +3,10 @@ from .models import *
 
 # Register your models here.
 admin.site.register(AreaOfInterest)
+admin.site.register(Inference)
 admin.site.register(BoundingBox)
-#admin.site.register(Inference)
+admin.site.register(Event)
+admin.site.register(Algorithm)
 admin.site.register(Label)
 admin.site.register(InferenceDetectionClassification)
 admin.site.register(InferenceClassification)
-admin.site.register(Event)
-admin.site.register(Alert)
