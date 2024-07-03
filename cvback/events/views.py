7302110f538c565d30e7db7a25cdc5f4cd4e5777
Paula Vallejo <paula.vallejo@gmail.com>
2024-07-03 11:36:40 -0400

    add: serializer and views

--- cvback/events/views.py
+++ cvback/events/views.py
@@ -3,8 +3,8 @@ from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.utils.decorators import method_decorator
 from rest_framework.parsers import JSONParser
-from cvback.events.models import AreaOfInterest, Event
-from cvback.events.serializers import EventSerializer, AreaOfInterestSerializer
+from cvback.events.models import Event
+from cvback.events.serializers import EventSerializer
 from django.views import View
 import json
 import logging
@@ -18,20 +18,30 @@ class MQTTEventView(View):
     def post(self, request, *args, **kwargs):
         try:
             payload = json.loads(request.body.decode('utf-8'))
-
-            data = {
-                'camera_id': payload.get('camera_id'),
-                'inference_detection_classification_id': payload.get('inference_detection_classification_id'),
-                'inference_classification_id': payload.get('inference_classification_id'),
-            }
-
-            event = Event.objects.create(
-                camera_id=data['camera_id'],
-                inference_detection_classification_id=data.get('inference_detection_classification_id'),
-                inference_classification_id=data.get('inference_classification_id'),
-            )
+            serializer = EventSerializer(data=payload)
             
-            return JsonResponse({'status': 'success', 'event_id': event.id}, status=201)
+            if serializer.is_valid():
+                event = Event.object.create(
+                    event_type_id=serializer.validated_data['event_type_id'],
+                    confidence=serializer.validated_data.get('confidence')
+                )
+                
+                camera_ids = serializer.validated_data.get('camera_ids', [])
+                event.cameras.add(*Camera.objects.filter(id__in=camera_ids))
+                
+                event.labels_detected.add(*serializer.validated_data.get('labels_detected', []))
+                event.labels_missing.add(*serializer.validated_data.get('labels_missing', []))
+                
+                event.key_inference_classification.add(*serializer.validated_data.get('key_inference_classification_ids', []))
+                event.key_inference_detection_classification.add(*serializer.validated_data.get('key_inference_detection_classification_ids', []))
+                event.key_inference_detection_classification_tracker.add(*serializer.validated_data.get('key_inference_detection_classification_tracker_ids', []))
+                event.key_inference_ocr.add(*serializer.validated_data.get('key_inference_ocr_ids', []))
+                
+                event.save()
+                
+                return JsonResponse({'status': 'success', 'event_id': event.id}, status=201)
+            else:
+                return JsonResponse({'status': 'error', 'errors': serializer.errors}, status=400)
 
         except json.JSONDecodeError as json_error:
             logger.error(f"JSON Decode Error: {json_error}")
@@ -40,49 +50,3 @@ class MQTTEventView(View):
         except Exception as e:
             logger.error(f"Error processing request: {e}", exc_info=True)
             return JsonResponse({'status': 'error', 'message': str(e)}, status=500)
-            
-@csrf_exempt
-def areas_of_interest_list(request):
-    """
-    List all code snippets, or create a new snippet.
-    """
-    if request.method == 'GET':
-        areas_of_interest = AreaOfInterest.objects.all()
-        serializer = AreaOfInterestSerializer(areas_of_interest, many=True)
-        return JsonResponse(serializer.data, safe=False)
-
-    elif request.method == 'POST':
-        data = JSONParser().parse(request)
-        serializer = AreaOfInterestSerializer(data=data)
-        if serializer.is_valid():
-            serializer.save()
-            return JsonResponse(serializer.data, status=201)
-        return JsonResponse(serializer.errors, status=400)
-
-
-
-@csrf_exempt
-def area_of_interest_detail(request, pk):
-    """
-    Retrieve, update or delete a code snippet.
-    """
-    try:
-        area_of_interest = AreaOfInterest.objects.get(pk=pk)
-    except AreaOfInterest.DoesNotExist:
-        return HttpResponse(status=404)
-
-    if request.method == 'GET':
-        serializer = AreaOfInterestSerializer(area_of_interest)
-        return JsonResponse(serializer.data)
-
-    elif request.method == 'PUT':
-        data = JSONParser().parse(request)
-        serializer = AreaOfInterestSerializer(area_of_interest, data=data)
-        if serializer.is_valid():
-            serializer.save()
-            return JsonResponse(serializer.data)
-        return JsonResponse(serializer.errors, status=400)
-
-    elif request.method == 'DELETE':
-        area_of_interest.delete()
-        return HttpResponse(status=204)
