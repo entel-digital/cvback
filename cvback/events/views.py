543bb86da22462a28153d624ea01864cc327585a
Paula Vallejo <paula.vallejo@gmail.com>
2024-07-07 19:42:57 -0400

    fix: keyframe model

--- cvback/events/views.py
+++ cvback/events/views.py
@@ -3,10 +3,8 @@ from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.utils.decorators import method_decorator
 from rest_framework.parsers import JSONParser
-from cvback.events.serializers import BoundingBoxSerializer, FrameSerializer, InferenceClassificationSerializer, InferenceDetectionClassificationSerializer, InferenceDetectionClassificationTrackerSerializer, InferenceOCRSerializer, VideoSerializer, EventSerializer
-from cvback.events.models import Frame
-from django.views import View
-from rest_framework.views import APIView
+from cvback.events.serializers import BoundingBoxSerializer, FrameSerializer, InferenceClassificationSerializer, InferenceDetectionClassificationSerializer, InferenceDetectionClassificationTrackerSerializer, InferenceOCRSerializer, VideoSerializer, EventSerializer, KeyFrameSerializer, LabelSerializer
+from cvback.events.models import Frame, Label, KeyFrame,BoundingBox, InferenceClassification, InferenceDetectionClassification, InferenceDetectionClassificationTracker, InferenceOCR, Event, Video
 from rest_framework.generics import ListCreateAPIView
 from rest_framework.response import Response
 from rest_framework import status
@@ -19,20 +17,12 @@ logger = logging.getLogger(__name__)
 
 
 @method_decorator(csrf_exempt, name='dispatch')
-class BoundingBoxApiView(APIView):
-    def post(self, request, format=None):
-        serializer = BoundingBoxSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-@method_decorator(csrf_exempt, name='dispatch')
-class FrameApiView(ListCreateAPIView):
-    model = Frame
-    queryset = Frame.objects.all()
-    serializer_class = FrameSerializer
+class BaseListCreateAPIView(ListCreateAPIView):
     
+    @classmethod
+    def __init__(self, *args,**kwargs ):
+        self.queryset = self.model.objects.all()
+
     def create(self, request, *args, **kwargs):
         if isinstance(request.data, list):
             serializer = self.get_serializer(data=request.data, many=True)
@@ -43,61 +33,43 @@ class FrameApiView(ListCreateAPIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-        
-@method_decorator(csrf_exempt, name='dispatch')
-class InferenceClassificationApiView(APIView):
-    def post(self, request, format=None):
-        serializer = InferenceClassificationSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
-@method_decorator(csrf_exempt, name='dispatch')
-class InferenceDetectionClassificationApiView(APIView):
-    def post(self, request, format=None):
-        serializer = InferenceDetectionClassificationSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
 
-@method_decorator(csrf_exempt, name='dispatch')
-class InferenceDetectionClassificationTrackerApiView(APIView):
-    def post(self, request, format=None):
-        serializer = InferenceDetectionClassificationTrackerSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
+class BoundingBoxApiView(BaseListCreateAPIView):
+    model = BoundingBox
+    serializer_class = BoundingBoxSerializer
 
-@method_decorator(csrf_exempt, name='dispatch')
-class InferenceOCRApiView(APIView):
-    def post(self, request, format=None):
-        serializer = InferenceOCRSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
+class FrameApiView(BaseListCreateAPIView):
+    model = Frame
+    serializer_class = FrameSerializer
 
-@method_decorator(csrf_exempt, name='dispatch')
-class EventApiView(APIView):
-    def post(self, request, format=None):
-        serializer = EventSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
+class KeyFrameApiView(BaseListCreateAPIView):
+    model = KeyFrame
+    serializer_class = KeyFrameSerializer
 
-@method_decorator(csrf_exempt, name='dispatch')
-class VideoApiView(APIView):
-    def post(self, request, format=None):
-        serializer = VideoSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+class InferenceClassificationApiView(BaseListCreateAPIView):
+    model = InferenceClassification
+    serializer_class = InferenceClassificationSerializer
+    
+class InferenceDetectionClassificationApiView(BaseListCreateAPIView):
+    model = InferenceDetectionClassification
+    serializer_class = InferenceDetectionClassificationSerializer
+
+class InferenceDetectionClassificationTrackerApiView(BaseListCreateAPIView):
+    model = InferenceDetectionClassificationTracker
+    serializer_class = InferenceDetectionClassificationTrackerSerializer
+
+class InferenceOCRApiView(BaseListCreateAPIView):
+    model = InferenceOCR
+    serializer_class = InferenceOCRSerializer
+
+class LabelApiView(BaseListCreateAPIView):
+    model = Label
+    serializer_class = LabelSerializer
+
+class EventApiView(BaseListCreateAPIView):
+    model = Event
+    serializer_class= EventSerializer
+
+class VideoApiView(BaseListCreateAPIView):
+    model = Video 
+    serializer_class = VideoSerializer
