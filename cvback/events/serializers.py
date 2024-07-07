df3a554cd71cf5c1745117d4e4e19e6c3d80194c
Paula Vallejo <paula.vallejo@gmail.com>
2024-07-07 19:44:21 -0400

    fix: KeyFrame typo

--- cvback/events/serializers.py
+++ cvback/events/serializers.py
@@ -1,8 +1,8 @@
 from rest_framework import serializers
 from cvback.events.models import BoundingBox, Frame, Video, InferenceDetectionClassificationTracker, InferenceDetectionClassification, InferenceOCR, Event
 #from drf_compound_fields.fields import ListField
-from cvback.events.models import AreaOfInterest, Event, InferenceDetectionClassification, InferenceClassification, Label
-
+from cvback.events.models import AreaOfInterest, Event, InferenceDetectionClassification, InferenceClassification, Label, KeyVideos, KeyInferenceClassification, KeyInferenceDetectionClassification, KeyInferenceDetectionClassificationTracker, KeyInferenceOCR, KeyFrame
+from cvback.devices.serializers import CameraSerializer
 # TODO: +Frames, +Videos, +Inference classification, +Inference detection classification tracker, +Inference detection classification, +Inference ocr, +Event
 class BoundingBoxSerializer(serializers.ModelSerializer):
     class Meta:
@@ -19,9 +19,19 @@ class InferenceClassificationSerializer(serializers.ModelSerializer):
         model = InferenceClassification
         fields = "__all__"
 
+class KeyInferenceClassificationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = KeyInferenceClassification
+        fields = "__all__"
+
 class InferenceDetectionClassificationSerializer(serializers.ModelSerializer):
     class Meta:
         model = InferenceDetectionClassification
+        fields = "__all__"
+
+class KeyInferenceDetectionClassificationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = KeyInferenceDetectionClassification
         fields = "__all__"
 
 class InferenceDetectionClassificationTrackerSerializer(serializers.ModelSerializer):
@@ -29,19 +39,51 @@ class InferenceDetectionClassificationTrackerSerializer(serializers.ModelSeriali
         model = InferenceDetectionClassificationTracker
         fields = "__all__"
 
+class KeyInferenceDetectionClassificationTrackerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = KeyInferenceDetectionClassificationTracker
+        fields = "__all__"
+
 class InferenceOCRSerializer(serializers.ModelSerializer):
     class Meta:
         model = InferenceOCR
         fields = "__all__"
 
-class EventSerializer(serializers.ModelSerializer):
+class KeyInferenceOCRSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Event
+        model = KeyInferenceOCR
         fields = "__all__"
+
 
 class VideoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Video
         fields = "__all__"
 
+class KeyVideosSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = KeyVideos
+        fields = "__all__"
+
+class LabelSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Label
+        fields = ("id",)
+
+
+class KeyFrameSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = KeyFrame
+        fields = "__all__"
+
+class EventSerializer(serializers.ModelSerializer):
+
+    cameras = CameraSerializer(many=True,read_only=True )
+
+    
+    labels_detected = LabelSerializer(required=False, many=True, read_only=True)
+    labels_missing = LabelSerializer(required=False, many=True,read_only=True)
+    class Meta:
+        model = Event
+        fields = "__all__"
 
