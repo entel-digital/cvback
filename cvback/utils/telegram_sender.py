41e756a3b26a857b14169218f3fc386fcb0411b3
Vera Sativa <hola@verasativa.com>
2024-07-10 23:00:10 -0400

    wip: enviroments fix

--- cvback/utils/telegram_sender.py
+++ cvback/utils/telegram_sender.py
@@ -1,8 +1,10 @@
 import requests
-import os
+import environ
+
+env = environ.Env()
 class TelegramSender():
-    token =  os.environ["TELEGRAM_BOT_TOKEN"]# TODO: env vars
-    def send_telegram(self, text, chat_id):    
+    token = env("TELEGRAM_BOT_TOKEN") # TODO: env vars
+    def send_telegram(self, text, chat_id):
         url = f"https://api.telegram.org/bot{self.token}/sendMessage?chat_id={chat_id}&text={text}"
-        response = requests.get(url).json() # this sends the message    
+        response = requests.get(url).json() # this sends the message
         return response
