692bb28190576ac0e149689eab11b3ac1b2a5bc2
natiacostap <natiacostap@gmail.com>
2024-07-11 02:53:51 -0400

    sorting events

--- vue_frontend/src/stores/global-store.js
+++ vue_frontend/src/stores/global-store.js
@@ -9,6 +9,9 @@ export const useGlobalStore = defineStore("global", {
     async FETCH_EVENTS() {
       try {
         const data = await getAllEvents();
+        data.allEvents.sort((a, b) => {
+          return new Date(b.addedDate) - new Date(a.addedDate);
+        });
         return data.allEvents;
       } catch (error) {
         console.log("HERE IN ERROR FETCH_EVENTS");
