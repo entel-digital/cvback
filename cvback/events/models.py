0f1d752095d4c4c185c6eb071d37c978b885e4d4
Vera Sativa <hola@verasativa.com>
2024-07-11 01:29:39 -0400

    fix: media path

--- cvback/events/models.py
+++ cvback/events/models.py
@@ -92,16 +92,12 @@ class Frame(models.Model):
 
     def get_image_url(self):
         if self.image:
-            request = self.context.get('request')
-            image_url = self.image.url
-            return request.build_absolute_uri(image_url)
+            return self.image.url
         return None
 
     def get_image_with_boundingboxes_url(self):
         if self.image_with_boundingboxes:
-            request = self.context.get('request')
-            image_url = self.image_with_boundingboxes.url
-            return request.build_absolute_uri(image_url)
+            return self.image_with_boundingboxes.url
         return None
 
 
