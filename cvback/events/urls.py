e21e968e40ea9a0a69719bb3e752cd79c86a2218
Vera Sativa <hola@verasativa.com>
2024-07-17 19:16:33 -0400

    test: graphQL disabled ++

--- cvback/events/urls.py
+++ cvback/events/urls.py
@@ -6,9 +6,8 @@ from cvback.schema import schema
 from django.views.decorators.csrf import csrf_exempt
 
 
-
 urlpatterns = [
-    #path('graphql/', GraphQLView.as_view(graphiql=True, schema=schema)),
+    # path('graphql/', GraphQLView.as_view(graphiql=True, schema=schema)),
     path('labels/', LabelApiView.as_view(), name="events_label"),
     path('bounding_box/', BoundingBoxApiView.as_view(), name='events_bounding_box'),
     path('frames/', FrameApiView.as_view(), name='events_frames'),
@@ -19,5 +18,4 @@ urlpatterns = [
     path('inference_ocr/', InferenceOCRApiView.as_view(), name='events_inference_ocr'),
     path('main_event/', EventApiView.as_view(), name='events_event'),
     path('videos/', VideoApiView.as_view(), name='events_videos'),
-
 ]
