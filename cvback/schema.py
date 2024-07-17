a64453ee23ae31c0d130a39897bed375210198c7
crahumadao <ahumada.860@gmail.com>
2024-07-17 17:31:55 -0400

    feature: pagination in events

--- cvback/schema.py
+++ cvback/schema.py
@@ -1,4 +1,5 @@
 import graphene
+from graphene_django.filter import DjangoFilterConnectionField
 from cvback.events.schema import (LabelType, KeyFrameType, BoundingBoxType, EventTypeType, EventType)
 from cvback.events.models import (Label, KeyFrame, BoundingBox, Event)
 
@@ -21,9 +22,23 @@ class Query(graphene.ObjectType):
     #all_labels = graphene.List(LabelType)
     #all_bounding_boxes = graphene.List(BoundingBoxType)
     #all_key_frames = graphene.List(KeyFrameType)
-
     def resolve_all_events(self, info):
-        return Event.objects.first()
+        return Event.objects.all()
+    
+    paginated_events = DjangoFilterConnectionField(
+        EventType,
+        first=graphene.Int(),
+        skip=graphene.Int()
+    )
+    def resolve_paginated_events(self, info, **kwargs):
+        qs = Event.objects.all()
+        skip = kwargs.get('skip')
+        first = kwargs.get('first')
+        if skip:
+            qs = qs[skip:]
+        if first:
+            qs = qs[:first]
+        return qs
 
 class Mutation(graphene.ObjectType):
     update_event = UpdateEventMutation.Field()
